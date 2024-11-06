e747fd80c22fa24f0d5123b117867427a7a0d611
Nevin Aquinas <87398867+neaqx@users.noreply.github.com>
2024-11-06 14:23:07 +0000

    Fix settings in this project

--- ammas-kitchen/settings.py
+++ ammas-kitchen/settings.py
@@ -11,10 +11,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-=dji^4@%kj@t_nn^j$ky%$)vs4iecjlfx=xcr(+wh-dk7ex)r="
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
-    '*'
+    'https://ammas-kitchen-3d49eddc3540.herokuapp.com/'
 ]
 
 # Application definition
